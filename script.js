792912fa39c267a2f3efcd104c90029215e30f56
johan morin <johan.morin@hotmail.fr>
2018-05-31 16:09:42 +0200

    Fix bugs on clearGame()

--- script.js
+++ script.js
@@ -7,27 +7,27 @@ const gameBoard = (() => {
 
     let gameBoardArray = ['', '', '', '', '', '', '', '', '']; // the array is looped over to create the html
 
-    const clearGameBoard = () => {
+    const clearGameBoard = function() {
         
-        gameBoardArray = ['', '', '', '', '', '', '', '', ''];
+        this.gameBoardArray = ['', '', '', '', '', '', '', '', ''];
     };
 
     // to add the player's mark in the gameBoardArray
-    const update = (position, currentPlayer) => {
-        gameBoardArray.splice(position, 1, currentPlayer.mark)
+    const update = function(position, currentPlayer) {
+        this.gameBoardArray.splice(position, 1, currentPlayer.mark)
     };
 
     // create the html to be displayed on the webpage
-    const createGameBoard = () => {
+    const createGameBoard = function() {
         let gridGameBoard = '';
 
-        for (let i = 0; i < gameBoardArray.length; i++) {
+        for (let i = 0; i < this.gameBoardArray.length; i++) {
 
-            if (gameBoardArray[i] === '') {
+            if (this.gameBoardArray[i] === '') {
 
-                gridGameBoard += `<div id='${i}' class='notPlayed playBox'>${gameBoardArray[i]}</div>`;
+                gridGameBoard += `<div id='${i}' class='notPlayed playBox'>${this.gameBoardArray[i]}</div>`;
 
-            } else gridGameBoard += `<div id='${i}' class='played playBox'>${gameBoardArray[i]}</div>`;
+            } else gridGameBoard += `<div id='${i}' class='played playBox'>${this.gameBoardArray[i]}</div>`;
 
         }
         return gameBoardDisplay.innerHTML = gridGameBoard;
@@ -43,13 +43,13 @@ const Player = (name, mark) => {
 
     let spotPlayed = [];
 
-    const clearPlayer = () => {
-        spotPlayed = [];
+    const clearPlayer = function() {
+        this.spotPlayed = [];
     };
 
-    const play = (id) => { // keep track of the played spots by the player
-
-        spotPlayed.push(id);
+    const play = function(id) {
+        this.spotPlayed.push(id);
+        console.log(this.spotPlayed);
     };
 
     return { name, mark, spotPlayed, play, clearPlayer };
@@ -58,13 +58,13 @@ const Player = (name, mark) => {
 // Creation of two players with names
 
 
-let player1 = Player('Pierre', 'X');
-let player2 = Player('Johan', 'O');
+let player1 = Player('Player 1', 'X');
+let player2 = Player('Player 2', 'O');
 
 // create the displayController with a module
 // this is going to control the all game
 const displayController = (() => {
-    let currentPlayer = player1;
+    let currentPlayer = null;
     let winner = null;
     let winningLine = null;
     let gameFinished = false;
@@ -73,7 +73,7 @@ const displayController = (() => {
     // check for a winner by checking each turn if the current player made a winning move
     // by checking the 'spotPlayed' against the winning combinations. If any winning combination
     // is contained in the player's 'spotPlayed' then he is the winner
-    const checkForAWinner = () => {
+    const checkForAWinner = function() {
 
         let winningCombinations = [['0', '1', '2'], ['3', '4', '5'], ['6', '7', '8'], ['0', '3', '6'], ['1', '4', '7'], ['2', '5', '8'], ['0', '4', '8'], ['2', '4', '6']];
 
@@ -84,23 +84,26 @@ const displayController = (() => {
                 winningLine = winningCombinations[i]; // return the winning line to then hilight it
                 return true;
             }
-
         }
-
     };
     // to change from one player to another
-    const setPlayer = () => {
+    const setPlayer = function() {
 
         if (gameStarted) {
 
             return currentPlayer === player1 ? currentPlayer = player2 : currentPlayer = player1;
 
-        } else return (currentPlayer, gameStarted = true);
+        } else return (currentPlayer = player1, gameStarted = true);
+    };
+
+    const announceNextPlayer = function() {
+        let nextPlayerDisplay = document.getElementById('result');
+        currentPlayer === player1 ? nextPlayerDisplay.textContent = `${player2.name}'s turn` : nextPlayerDisplay.textContent = `${player1.name}'s turn`;
     };
 
     // to add the eventListeners only to the spots that are not played yet
     // thus a player can't play on an already played spot
-    const printGame = () => {
+    const printGame = function() {
 
         gameBoard.createGameBoard();
         let spotsToPlay = document.querySelectorAll('.notPlayed');
@@ -111,18 +114,20 @@ const displayController = (() => {
 
         }));
 
-    }
+    };
 
-    const playGame = (event) => {
+    const playGame = function(event) {
 
         if (!gameFinished) {
+
             setPlayer(); // select current player
 
             gameBoard.update(event.target.id, currentPlayer); // update of the gameBoard
 
             currentPlayer.play(event.target.id); // keep track of this player's move
-
             printGame(); // display the game on the webpage
+
+            
 
             if (checkForAWinner()) { // check for a winner each turn
 
@@ -135,32 +140,44 @@ const displayController = (() => {
 
                 gameFinished = true;
             };
+
+            if (!gameFinished) announceNextPlayer();
         };
     };
 
-    // This doesn't work
-    // it doesn't update anything when I call it from the console.
+    // to start a new game and clear everything
     const clearGame = () => {
-
         player1.clearPlayer();
         player2.clearPlayer();
-
         gameBoard.clearGameBoard();
-        console.log(gameBoard.gameBoardArray);
-        
+
+        winner = null;        
         gameFinished = false;
         gameStarted = false;
         winningLine = null;
-        currentPlayer = player1;
+
+        document.getElementById('result').textContent = '';
         printGame();
-
-
     };
 
 
     return { playGame, printGame, clearGame };
 })();
 
+
+// Public stuff
+
+function addPlayer() {
+
+    if (event.target.id == 'addPlayer1') {
+        let name = document.getElementById('player1').value;
+        player1 = Player(name, 'X');
+
+    } else {
+        let name = document.getElementById('player2').value;
+        player2 = Player(name, 'O');
+    }
+}
 
 // functions to find a place for
 
